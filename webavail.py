74d44baaeb89c00ae9d81d7cb9752986065e5589
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-11-30 12:52:30 -0500

    Adding function shell and description to check if url is well formed

--- webavail.py
+++ webavail.py
@@ -3,19 +3,31 @@ import dns.resolver
 import subprocess
 import re
 
+def check_input_url(input_url):
+    '''
+    Checks the input url that it conforms to the standard format of
+    <subdomain>.<domain>.<top-level domain> standard.
+
+    Returns 0 if well-formed.
+    Returns 1 if ill-formed.
+    '''
+
+    return 0
+
 def main():
     #Check the input to make sure that a parameter was passed
     try:
-        print sys.argv[1]
-        sys.exit(0)
-    except IndexError as e:
-        print "IndexError: {}".format(e)
+        input_url = sys.argv[1]
+    except IndexError:
+        print "No url parameter passed. Please provide a url."
         sys.exit(1)
 
     #Check the input URL to confirm that it is well formed
+    if check_input_url(input_url):
+        print "URL is not well formed. Please provide a well formed url."
+        sys.exit(1)
 
     #Make an http request to the website to confirm that it is available
-
     #If unavailable, print message that it's unavailable
 
     #If available, check DNS records for all A record IPs and NS records
@@ -25,6 +37,8 @@ def main():
     #pythons subprocess module
 
     #Print this information to the console and exit the utility
+
+    print input_url
 
 if __name__ == "__main__":
     main()
