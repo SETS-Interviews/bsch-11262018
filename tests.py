a82820cb8e37d3e9d239dc5043213a13a6d1009e
Brian Schuster <brian.daniel.schuster@gmail.com>
2018-12-01 08:52:08 -0500

    Altering tests to reflect changes in check_input_url

--- tests.py
+++ tests.py
@@ -3,20 +3,20 @@ import sys
 
 #Check Input URL
 bad_url_1 = 'reddit'
-assert check_input_url(bad_url_1) == 1
+assert check_input_url(bad_url_1)[0] == 1
 bad_url_2 = 'reddit.com'
-assert check_input_url(bad_url_1) == 1
+assert check_input_url(bad_url_1)[0] == 1
 bad_url_3 = 'google.notarealdomain'
-assert check_input_url(bad_url_3) == 1
+assert check_input_url(bad_url_3)[0] == 1
 
 good_url_1 = "http://www.google.com"
-assert check_input_url(good_url_1) == 0
+assert check_input_url(good_url_1)[0] == 0
 good_url_2 = "http://api.reddit.com"
-assert check_input_url(good_url_1) == 0
+assert check_input_url(good_url_1)[0] == 0
 good_url_3 = "https://www.google.com"
-assert check_input_url(good_url_3) == 0
+assert check_input_url(good_url_3)[0] == 0
 good_url_4 = 'https://www.reddit.com/r/programming/'
-assert check_input_url(good_url_4) == 0
+assert check_input_url(good_url_4)[0] == 0
 
 #Check Result of URL
 url_check_1 = "madeupnonsense"
